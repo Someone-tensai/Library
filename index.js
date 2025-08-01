feac146b1055ce383c98e190b4c88681e91bb42c
Someone-tensai <paudelsulav5@gmail.com>
2025-08-01 21:30:57 +0545

    Added Dialog Option(Not functional yet)

--- index.js
+++ index.js
@@ -18,33 +18,57 @@ function add_book(name, author, year)
     let new_book = new Book(name, author, year);
     my_library.push(new_book);
 }
-
-
+let container = document.querySelector(".container");
+let body = document.querySelector("body");
 
 document.addEventListener("DOMContentLoaded",function display_books()
 {
     add_book("My Book" , "Me", "1949");
     add_book("1984", "George Orwell", "1984");
+
     console.log(my_library);
-    let container = document.querySelector(".container");
-    let body = document.querySelector("body");
+
+  
+ 
+
     let head = document.createElement("h2");
+
     head.textContent = "Your Books";
     body.prepend(head);
+
     for(const ind_book of my_library)
     {
         console.log(ind_book);
         let book = document.createElement("div");
         let title = document.createElement("div");
+
         title.classList.add("title");
         book.classList.add("book");
+
         title.textContent = `${ind_book.name}`;
         book.textContent += `Author: ${ind_book.author}\n`;
         book.textContent += `Year of Publication: ${ind_book.year}`;
+
         book.prepend(title);
-       
-       
         container.appendChild(book);
     }
     
-})
+});
+
+
+let new_button = document.querySelector(".new");
+new_button.addEventListener("click", ()=>
+{
+    let dialog = document.createElement("dialog");
+    let form = document.querySelector("form");
+    form.hidden = false;
+    dialog.appendChild(form);
+    container.appendChild(dialog);
+    dialog.showModal();
+});
+
+let form_submit = document.querySelector(".submit");
+form_submit.addEventListener("click", ()=>
+{
+    let name = document.querySelector("name")
+});
