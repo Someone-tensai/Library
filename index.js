c3a037dd895d22aaf940987a6f4def23a2393d52
Someone-tensai <paudelsulav5@gmail.com>
2025-08-03 22:48:22 +0545

    Remove Button/ Toggle Button

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const my_library = [];
+let my_library = [];
+let div_new = document.querySelector(".button");
+let container = document.querySelector(".container");
+let body = document.querySelector("body");
+let new_button = document.querySelector(".new");
+let form = document.querySelector("form");
+let dialog = document.createElement("dialog");
+let remove = document.querySelector(".remove");
+let form_submit = document.querySelector(".submit");
+
+
+document.addEventListener("DOMContentLoaded", display_books);
 
 function Book(name, author, year) {
   if (!new.target) {
@@ -8,68 +19,91 @@ function Book(name, author, year) {
   this.name = name;
   this.author = author;
   this.year = year;
-  this.displayed = false;
+  this.read = false;
+  this.toggle_status = function()
+    {
+        if(this.read === false) this.read = true;
+        else this.read = false;
+    }
 }
 
 function add_book(name, author, year) {
   let new_book = new Book(name, author, year);
   my_library.push(new_book);
-  console.log(my_library);
 }
-
-let div_new = document.querySelector(".button");
-
+add_book("Something", "Someone" , 1984);
 function display_books() {
   console.log(my_library);
-
-  for (const ind_book of my_library) {
-    if(ind_book["displayed"] === true) continue;
+  container.innerHTML = "";
+  
+    for (const ind_book of my_library) {
     console.log(ind_book);
     let book = document.createElement("div");
     let title = document.createElement("div");
-
+    let remove_button = document.createElement("button");
+    let toggle_button = document.createElement("button");
+    toggle_button.textContent = "Toggle Status";
+    toggle_button.classList.add("toggle");
+    remove_button.classList.add("remove");
+    remove_button.textContent = "Remove";
+    toggle_button.dataset.id = ind_book.id;
+    remove_button.dataset.id = ind_book.id;
     title.classList.add("title");
     book.classList.add("book");
 
     title.textContent = `${ind_book.name}`;
     book.textContent += `Author: ${ind_book.author}\n`;
     book.textContent += `Year of Publication: ${ind_book.year}`;
+    book.textContent += ind_book.read == true?`\nStatus: Read\n`:`\nStatus: Not Read\n`;
 
     book.prepend(title);
+    book.append(remove_button);
+    book.append(toggle_button);
     container.appendChild(book);
-    ind_book["displayed"] = true;
   }
   body.appendChild(container);
   body.appendChild(div_new);
 }
-let container = document.querySelector(".container");
-let body = document.querySelector("body");
-
-document.addEventListener("DOMContentLoaded", display_books);
-
-let new_button = document.querySelector(".new");
-let form = document.querySelector("form");
-let dialog = document.createElement("dialog");
 
 new_button.addEventListener("click", () => {
+
   form.hidden = false;
   dialog.appendChild(form);
   container.appendChild(dialog);
   dialog.showModal();
+
 });
 
-let form_submit = document.querySelector(".submit");
 form_submit.addEventListener("click", (event) => {
+
   event.preventDefault();
-  console.log("CLicked");
+
   let name = document.querySelector(".name");
   let author = document.querySelector(".author");
   let year = document.querySelector(".year");
-  console.log(name.value);
-  console.log(author.value);
-  console.log(year.value);
+
   add_book(name.value, author.value, year.value);
+
   dialog.close();
+  form.reset();
+  display_books();
+
+});
+
+container.addEventListener("click", (event) => {
+  if (event.target.closest(".remove"))
+  {
+  my_library = my_library.filter((obj) => {
+    return obj.id != event.target.dataset.id;
+  });
+  }
+    else if(event.target.closest(".toggle"))
+    {
+    my_library.filter((obj) => {
+        return obj.id == event.target.dataset.id;
+        })[0].toggle_status();
+    }
+    else return;
   display_books();
 
 });
